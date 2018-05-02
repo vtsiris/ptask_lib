9d1713d95e288bebbdcc440f0b71f20d4831b24e
Vaggelis Tsiris <tzelakos@gmail.com>
2018-05-02 17:29:42 +0300

    fix()app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,10 @@ import { ServicesService } from './services.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
-  bookSelected: boolean;
 
   constructor(private service: ServicesService) { }
 
   ngOnInit() {
-    this.service._bookToggle.subscribe(x => this.bookSelected = x);
+
   }
 }
